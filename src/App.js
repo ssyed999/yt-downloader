e9a6afc756c04fdf88d6cd70652de5d9882edf07
Kumar Sanu <kumsanu@WKMZTE07F168.global.publicisgroupe.net>
2022-10-25 21:24:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import Typed from "typed.js";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [videoUrl, setVideoUrl] = useState("");
+  const [source, setSource] = useState("");
+  // Create reference to store the DOM element containing the animation
+  const el = useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = useRef(null);
+
+  useEffect(() => {
+    const url = `https://convert2mp3s.com/api/widget?url=${videoUrl}`;
+    setSource(url);
+  }, [videoUrl]);
+
+  useEffect(() => {
+    const options = {
+      strings: ["Youtube", "Instagram", "Facebook"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true,
+      loopCount: Infinity,
+    };
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setVideoUrl(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="type-wrap">
+        Download video/audio from :{" "}
+        <span style={{ whiteSpace: "pre" }} ref={el} />
+      </div>
+      <input
+        className="input"
+        type="text"
+        onChange={handleChange}
+        placeholder="Enter Video Url"
+      />
+      {videoUrl ? (
+        <iframe
+          title="downloader"
+          className="iframe"
+          src={source}
+          allowtransparency={true}
+          scrolling={true}
+        ></iframe>
+      ) : null}
     </div>
   );
 }
-
-export default App;
