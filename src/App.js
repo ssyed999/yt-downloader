01bbdff3aaa45d5af6ff98e3a982f76e8476ec2b
sadaf-GFT <sadaf.mushtaque@goldenfuturetechnologies.com.au>
2022-10-31 21:18:37 +0530

    fixed alignments 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import Typed from "typed.js";
 import { BrowserRouter } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import "./App.css";
 import Footer from "./footer/Footer";
+import BackgroundImage from "../src/assets/background3.jpg";
 import Logo from "../src/assets/Logo3.png";
 export default function App() {
   const [videoUrl, setVideoUrl] = useState("");
   const [source, setSource] = useState("");
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+
+    await loadFull(engine);
+  }, []);
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   // Create reference to store the DOM element containing the animation
   const el = useRef(null);
   // Create reference to store the Typed instance itself
@@ -43,6 +54,219 @@ export default function App() {
 
   return (
     <div className="App container scrollbar scrollbar-primary">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        // options={{
+        //   background: {
+        //     color: {
+        //       value: "#fffff",
+        //       // value: "#90ee90",
+        //     },
+        //   },
+        //   fpsLimit: 120,
+        //   interactivity: {
+        //     events: {
+        //       onClick: {
+        //         enable: true,
+        //         mode: "push",
+        //       },
+        //       onHover: {
+        //         enable: true,
+        //         mode: "repulse",
+        //       },
+        //       resize: true,
+        //     },
+        //     modes: {
+        //       push: {
+        //         quantity: 4,
+        //       },
+        //       repulse: {
+        //         distance: 200,
+        //         duration: 0.4,
+        //       },
+        //     },
+        //   },
+        //   particles: {
+        //     color: {
+        //       value: "#ffffff",
+        //     },
+        //     links: {
+        //       color: "#ffffff",
+        //       distance: 150,
+        //       enable: true,
+        //       opacity: 0.5,
+        //       width: 1,
+        //     },
+        //     collisions: {
+        //       enable: true,
+        //     },
+        //     move: {
+        //       directions: "none",
+        //       enable: true,
+        //       outModes: {
+        //         default: "bounce",
+        //       },
+        //       random: false,
+        //       speed: 6,
+        //       straight: false,
+        //     },
+        //     number: {
+        //       density: {
+        //         enable: true,
+        //         area: 800,
+        //       },
+        //       value: 80,
+        //     },
+        //     opacity: {
+        //       value: 0.5,
+        //     },
+        //     shape: {
+        //       type: "circle",
+        //     },
+        //     size: {
+        //       value: { min: 1, max: 5 },
+        //     },
+        //   },
+        //   detectRetina: true,
+        // }}
+        options={{
+          fullScreen: {
+            enable: true,
+            zIndex: -1,
+          },
+          particles: {
+            number: {
+              value: 200,
+              limit: 300,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000",
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+              image: {
+                src: "images/github.svg",
+                width: 100,
+                height: 100,
+              },
+            },
+            opacity: {
+              value: 0.5,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.5,
+                sync: false,
+              },
+            },
+            size: {
+              value: 30,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 10,
+                size_min: 10,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 100,
+              color: "#ffffff",
+              opacity: 1,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 3,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onHover: {
+                enable: true,
+                mode: "bubble",
+                parallax: {
+                  enable: false,
+                  force: 60,
+                  smooth: 10,
+                },
+              },
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                lineLinked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 400,
+                size: 100,
+                duration: 2,
+                opacity: 1,
+                speed: 2,
+              },
+              repulse: {
+                distance: 200,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          backgroundMask: {
+            enable: true,
+            cover: {
+              color: {
+                value: {
+                  r: 0,
+                  g: 0,
+                  b: 0,
+                },
+              },
+            },
+          },
+          retina_detect: true,
+          fps_limit: 60,
+          background: {
+            image: `url(${BackgroundImage})`,
+          },
+        }}
+      />
       <div className="header d-flex align-items-center ">
         <img src={Logo} style={{ height: "74px" }} alt="icon"></img>
         <h3 className="align-self-center flex-grow-1">
@@ -50,6 +274,14 @@ export default function App() {
           from any website{" "}
         </h3>
       </div>
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">Download Video/Audio</li>
+            <li className="nav-item">Extract audio from video link</li>
+          </ul>
+        </div>
+      </nav> */}
       <BrowserRouter>
         {videoUrl === "" && (
           <div className="type-wrap">
@@ -71,7 +303,11 @@ export default function App() {
             allowtransparency={true}
             scrolling={true}
           ></iframe>
-        ) : null}
+        ) : (
+          <div style={{ color: "white" }}>
+            Wait while we are proccessing you request.
+          </div>
+        )}
 
         <Footer />
       </BrowserRouter>
